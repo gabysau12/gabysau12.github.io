5187eab28f3dc0c302b0695c4fcdd6c2d4e5a4c8
gabysau12 <156268342+gabysau12@users.noreply.github.com>
2024-01-25 06:20:29 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded',function(){
-  console.log("Hello,world!");
- 
-	const formulario = document.getElementById('formulario');
+const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,8}$/, // 4 a 8 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
 const campos = {
+	
 	nombre: false,
 	password: false,
-	correo: false
+	correo: false,
+	
 }
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
+		
 		case "nombre":
 			validarCampo(expresiones.nombre, e.target, 'nombre');
 		break;
@@ -32,6 +32,7 @@ const validarFormulario = (e) => {
 		case "correo":
 			validarCampo(expresiones.correo, e.target, 'correo');
 		break;
+		
 	}
 }
 
@@ -52,7 +53,8 @@ const validarCampo = (expresion, input, campo) => {
 		campos[campo] = false;
 	}
 }
-	const validarPassword2 = () => {
+
+const validarPassword2 = () => {
 	const inputPassword1 = document.getElementById('password');
 	const inputPassword2 = document.getElementById('password2');
 
@@ -82,7 +84,7 @@ formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const terminos = document.getElementById('terminos');
-	if(campos.nombre && campos.password && campos.correo && terminos.checked ){
+	if( campos.nombre && campos.password && campos.correo && terminos.checked ){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
@@ -97,4 +99,3 @@ formulario.addEventListener('submit', (e) => {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 	}
 });
-
