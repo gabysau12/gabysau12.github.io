91b0bf56a65a371517937b931bbdea6dd2477fab
gabysau12 <156268342+gabysau12@users.noreply.github.com>
2024-01-25 01:36:43 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,3 @@
-document.addEventListener('DOMContentLoaded',function(){
-  console.log("Hello,world!");
-  var inputEmail = document.getElementById('email');
-  var inputPassword = document.getElementById ('password');
-  console.log(inputEmail);
-  console.log(inputPassword);
 
   const nombre = document.getElementBtId("name")
   
